fa9c45cbb083bb488072e7e132974bb975463c89
citrusqwe <citrusqwe@mail.ru>
2022-07-07 16:21:51 +0300

    add routing and 404 page

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -3,12 +3,41 @@ import {CommonModule} from '@angular/common';
 import {PaginationComponent} from "./pagination/pagination.component";
 import {BookComponent} from './book/book.component';
 import {HeaderComponent} from "./header/header.component";
-import { FirstPageComponent } from './pages/first-page/first-page.component';
-import { SecondPageComponent } from './pages/second-page/second-page.component';
-import { ThirdPageComponent } from './pages/third-page/third-page.component';
-import { FourthPageComponent } from './pages/fourth-page/fourth-page.component';
-import { FifthPageComponent } from './pages/fifth-page/fifth-page.component';
+import {FirstPageComponent} from './pages/first-page/first-page.component';
+import {SecondPageComponent} from './pages/second-page/second-page.component';
+import {ThirdPageComponent} from './pages/third-page/third-page.component';
+import {FourthPageComponent} from './pages/fourth-page/fourth-page.component';
+import {FifthPageComponent} from './pages/fifth-page/fifth-page.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {RouterModule, Routes} from "@angular/router";
+import {LayoutComponent} from './layout/layout.component';
 
+const routes: Routes = [
+  {
+    path: 'book', component: LayoutComponent, children: [
+      {
+        path: '1', component: FirstPageComponent
+      }, {
+        path: '2', component: SecondPageComponent
+      }, {
+        path: '3', component: ThirdPageComponent
+      }, {
+        path: '4', component: FourthPageComponent
+      }, {
+        path: '5', component: FifthPageComponent
+      },
+      {
+        path: '', redirectTo: '1', pathMatch: 'full'
+      },
+    ]
+  },
+  {
+    path: '', redirectTo: 'book', pathMatch: 'full'
+  },
+  {
+    path: '**', component: NotFoundComponent
+  },
+]
 
 @NgModule({
   declarations: [
@@ -19,12 +48,15 @@ import { FifthPageComponent } from './pages/fifth-page/fifth-page.component';
     SecondPageComponent,
     ThirdPageComponent,
     FourthPageComponent,
-    FifthPageComponent
+    FifthPageComponent,
+    NotFoundComponent,
+    LayoutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  exports: [BookComponent]
+  exports: [BookComponent, NotFoundComponent]
 })
 export class BookModule {
 }
